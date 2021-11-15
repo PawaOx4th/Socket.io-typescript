1f73cdc29281ceefa6138b6851ddbb706b85fbf4
pawaox4th <pawaox4th@gmail.com>
2021-11-15 15:18:38 +0700

    feat : 🍻 Broad cast content msg to room

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,24 @@ import dayjs from "dayjs"
 import "dayjs/locale/th"
 import utc from "dayjs/plugin/utc"
 import timezome from "dayjs/plugin/timezone"
+import { v4 as uuidv4 } from "uuid"
 
 dayjs.extend(utc)
 dayjs.extend(timezome)
+
+function timeStemp() {
+  return dayjs().tz("Asia/Bangkok").locale("th").format("DD/MMMM/YYYY HH:mm:ss")
+}
 
 const app = express()
 
 const httpServer = createServer(app)
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
+    // origin: "http://localhost:3000",
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true
   }
 })
 
@@ -32,7 +39,19 @@ app.get("/", (req: Request, res: Response) => {
 
 io.on("connection", (socket: Socket) => {
   // ...
-  console.log("😊 Use conenct :", socket.id)
+  console.log(`🔥 User join : ${socket.id} ${timeStemp()}`)
+
+  socket.on("TEST", (msg: string) => {
+    console.log("MSG", msg)
+
+    const fromDate = {
+      msgID: uuidv4(),
+      time: dayjs().unix(),
+      content: ` 🚨 FROM SERVER : ${msg}`
+    }
+
+    socket.emit("TEST", fromDate)
+  })
 })
 
 httpServer.listen(2000, () => {
